6ae6e35e6e8d030bc2c7828f67be6d89955b9288
pooja19purohit <poojaap66@gmail.com>
2016-04-28 21:34:54 -0400

    Command to search & filter

--- src/main/java/com/recipe/command/SearchRecipeCommand.java
+++ src/main/java/com/recipe/command/SearchRecipeCommand.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 
 import org.bson.Document;
+import org.bson.types.ObjectId;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
@@ -23,7 +24,7 @@ import java.util.List;
 
 public class SearchRecipeCommand {
 	ObjectMapper mapper = new ObjectMapper();
-	List<String> recipeAttributes = Arrays.asList("name", "difficultyLevel", "recipeCategory" , "cuisine", "prepTime", "cookTime", "ingredients", "direction", "additionalNote", "yields");
+	//List<String> recipeAttributes = Arrays.asList("name", "difficultyLevel", "recipeCategory" , "cuisine", "prepTime", "cookTime", "ingredients", "direction", "additionalNote", "yields");
 
 	public ArrayList<Recipe> execute(String query) {
 		MongoClient client = (new ConnectionProvider()).getConnection();
@@ -39,6 +40,42 @@ public class SearchRecipeCommand {
 		    searchQuery.put("text", query); */
 		    Document textSearch = new Document("$text", new Document("$search", query));
 			FindIterable<Document> cursor = recipeCollection.find(textSearch);
+			for (Document c : cursor) {
+				Recipe b = mapper.convertValue(c, Recipe.class);
+
+				recipes.add(b);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		}
+		finally{
+			client.close();
+		}
+		return recipes;
+
+	}
+	
+	public ArrayList<Recipe> execute(String query, String filterField, String filterValue) {
+		MongoClient client = (new ConnectionProvider()).getConnection();
+		MongoDatabase mdb = client.getDatabase("recipe");
+		MongoCollection<Document> recipeCollection = mdb.getCollection("recipeData");
+		
+		recipeCollection.createIndex(new BasicDBObject("$**","text"));
+		ArrayList<Recipe> recipes = new ArrayList<Recipe>();
+		
+		BasicDBObject searchQuery = new BasicDBObject();
+		if (filterField.equals("_id")) {
+			searchQuery.put(filterField, new ObjectId(filterValue));
+		} else {
+			searchQuery.put(filterField, filterValue);
+		}
+		
+		try {
+			/*BasicDBObject searchQuery = new BasicDBObject();      
+		    searchQuery.put("text", query); */
+		    Document textSearch = new Document("$text", new Document("$search", query));
+			FindIterable<Document> cursor = recipeCollection.find(textSearch).filter(searchQuery);
 			for (Document c : cursor) {
 				Recipe b = mapper.convertValue(c, Recipe.class);
 
