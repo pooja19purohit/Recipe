3415a906fcb895b93da484aee1ef993c465aa5ee
pooja19purohit <poojaap66@gmail.com>
2016-05-03 17:56:22 -0400

    Properties file

--- src/main/java/com/recipe/util/PropertiesLookup.java
+++ src/main/java/com/recipe/util/PropertiesLookup.java
@@ -12,7 +12,7 @@ public class PropertiesLookup {
 
 	public PropertiesLookup() {
 		try {
-			input = getClass().getResourceAsStream("config.properties");
+			input = getClass().getClassLoader().getResourceAsStream(filename);
 			if (input == null) {
 				throw new FileNotFoundException("Sorry, unable to find " + filename);
 			}
